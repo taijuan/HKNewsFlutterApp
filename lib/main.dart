cc17e601e22f2a5d8d832dd85722b10de8cd75c1
zhengtaijuan <673636090@qq.com>
2019-09-27 16:52:56 +0800

    1、状态栏字体颜色设置 2、Flutter run 启动界面问题

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hknews/Home.dart';
 import 'package:hknews/Welcome.dart';
 import 'package:hknews/localization/HKNewsLocalizationsDelegate.dart';
 
 void main() {
+  const SystemUiOverlayStyle dark = SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarDividerColor: Colors.transparent,
+    statusBarColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.light,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
+  );
+  SystemChrome.setSystemUIOverlayStyle(dark);
   return runApp(
     MaterialApp(
       theme: ThemeData(platform: TargetPlatform.iOS),
